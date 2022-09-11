a885f22f7e8ad7cf1120345b5df3382225b334cb
itschu <palmerbideconcepts@gmail.com>
2022-09-11 08:39:06 +0100

    fixing bug -3

--- middleware.js
+++ middleware.js
@@ -59,72 +59,72 @@ export default async function middleware(request, response) {
 	// 	return NextResponse.next();
 	// }
 
-	if (request.nextUrl.pathname.startsWith("/account")) {
-		if (jwt === undefined) {
-			return NextResponse.redirect(new URL("/login", request.url));
-		}
+	// if (request.nextUrl.pathname.startsWith("/account")) {
+	// 	if (jwt === undefined) {
+	// 		return NextResponse.redirect(new URL("/login", request.url));
+	// 	}
 
-		try {
-			const req = await fetch(`${domain}/api/loggedin`, {
-				method: "POST",
-				"Content-Type": "application/json",
-				Accept: "application/json",
-				body: JSON.stringify({ jwt }),
-			});
+	// 	try {
+	// 		const req = await fetch(`${domain}/api/loggedin`, {
+	// 			method: "POST",
+	// 			"Content-Type": "application/json",
+	// 			Accept: "application/json",
+	// 			body: JSON.stringify({ jwt }),
+	// 		});
 
-			const res = await req.json();
-			if (res.user.account == "user") {
-				return NextResponse.next();
-			} else {
-				return NextResponse.redirect(new URL("/vendor", request.url));
-			}
-		} catch (error) {
-			return NextResponse.redirect(new URL("/login", request.url));
-		}
-	}
+	// 		const res = await req.json();
+	// 		if (res.user.account == "user") {
+	// 			return NextResponse.next();
+	// 		} else {
+	// 			return NextResponse.redirect(new URL("/vendor", request.url));
+	// 		}
+	// 	} catch (error) {
+	// 		return NextResponse.redirect(new URL("/login", request.url));
+	// 	}
+	// }
 
-	if (request.nextUrl.pathname.startsWith("/vendor")) {
-		if (jwt === undefined) {
-			return NextResponse.redirect(new URL("/login", request.url));
-		}
+	// if (request.nextUrl.pathname.startsWith("/vendor")) {
+	// 	if (jwt === undefined) {
+	// 		return NextResponse.redirect(new URL("/login", request.url));
+	// 	}
 
-		try {
-			const req = await fetch(`${domain}/api/loggedin`, {
-				method: "POST",
-				"Content-Type": "application/json",
-				Accept: "application/json",
-				body: JSON.stringify({ jwt }),
-			});
+	// 	try {
+	// 		const req = await fetch(`${domain}/api/loggedin`, {
+	// 			method: "POST",
+	// 			"Content-Type": "application/json",
+	// 			Accept: "application/json",
+	// 			body: JSON.stringify({ jwt }),
+	// 		});
 
-			const res = await req.json();
-			if (res.user.account == "vendor") {
-				return NextResponse.next();
-			} else {
-				return NextResponse.redirect(new URL("/account", request.url));
-			}
-		} catch (error) {
-			return NextResponse.redirect(new URL("/login", request.url));
-		}
-	}
+	// 		const res = await req.json();
+	// 		if (res.user.account == "vendor") {
+	// 			return NextResponse.next();
+	// 		} else {
+	// 			return NextResponse.redirect(new URL("/account", request.url));
+	// 		}
+	// 	} catch (error) {
+	// 		return NextResponse.redirect(new URL("/login", request.url));
+	// 	}
+	// }
 
-	if (request.nextUrl.pathname.startsWith("/checkout")) {
-		if (jwt === undefined) {
-			return NextResponse.redirect(new URL("/login", request.url));
-		}
+	// if (request.nextUrl.pathname.startsWith("/checkout")) {
+	// 	if (jwt === undefined) {
+	// 		return NextResponse.redirect(new URL("/login", request.url));
+	// 	}
 
-		try {
-			const req = await fetch(`${domain}/api/loggedin`, {
-				method: "POST",
-				"Content-Type": "application/json",
-				Accept: "application/json",
-				body: JSON.stringify({ jwt }),
-			});
+	// 	try {
+	// 		const req = await fetch(`${domain}/api/loggedin`, {
+	// 			method: "POST",
+	// 			"Content-Type": "application/json",
+	// 			Accept: "application/json",
+	// 			body: JSON.stringify({ jwt }),
+	// 		});
 
-			return NextResponse.next();
-		} catch (error) {
-			return NextResponse.redirect(new URL("/login", request.url));
-		}
-	}
+	// 		return NextResponse.next();
+	// 	} catch (error) {
+	// 		return NextResponse.redirect(new URL("/login", request.url));
+	// 	}
+	// }
 
 	return NextResponse.next();
 }
